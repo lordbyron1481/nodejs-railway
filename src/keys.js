5f7b3fd3fdf8d1396fe95bd8d5a058106351b6ac
LORD BYRON <brodriguezmarrugo@gmail.com>
2022-10-10 14:55:03 -0500

    primera4

--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,7 @@
 module.exports={
 
     database:{
-        URI:'mongodb://localhost/mundoviral'
+        //URI:'mongodb://localhost/mundoviral'
+        URI:'mongodb+srv://lordbyron1481:<Bayron81071411664>@cluster0.cisorbb.mongodb.net/test'
     }
 };
